9a8cb1765da12c7cdc9105f93fc33f39128b1bb1
MehdiREZZAT <90774216+MehdiREZZAT@users.noreply.github.com>
2021-09-15 09:27:09 -0400

    Add files via upload

---
+++ tst.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <form method = "POST" action = "exe1.php">
+        entrer votre login : <input type="text" namespace= "login">
+        entrer votre mot de passe : <input type ="text" namespace= "passeword"
+        input type = "submit" name = "validation" value ="login"
+        input type = "submit" name = "register" value = "enregistrer"
+    </form>
+</body>
+</html>
